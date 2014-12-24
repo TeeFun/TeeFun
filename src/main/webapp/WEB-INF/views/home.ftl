367bc574e9247ca4dbb61e0e5e4a6ebb71616248
Rajh <Hetmoteus@gmail.com>
2014-12-24 18:07:35 +0100

    Fixed js

--- src/main/webapp/WEB-INF/views/home.ftl
+++ src/main/webapp/WEB-INF/views/home.ftl
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
-
 	<head>
+		<!-- Title -->
 		<title>Teeworlds Matchmaking System</title>
 
 		<!-- CSS -->
@@ -19,8 +19,8 @@
 		<#include "/home/content.ftl" />
 
 		<!-- Load js at end -->
-	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	    <!-- Bootstrap -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<!-- Bootstrap -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 
 		<!-- Common -->
--- src/main/webapp/resources/js/home/home.js
+++ src/main/webapp/resources/js/home/home.js
@@ -1,72 +1,68 @@
-<script type="text/javascript">
+// Refresh content every X seconds
+var refreshQueues = function () {
+$.get( "refreshQueues.do", function( data ) {
+		$("#queues").html( data );
+	});
+};
+setTimeout(function(){
+	refreshQueues();
+}, 15000);
 
-	// Refresh content every X seconds
-	var refreshQueues = function () {
-	$.get( "refreshQueues.do", function( data ) {
-			$("#queues").html( data );
-		});
-	};
-	setTimeout(function(){
-		refreshQueues();
-	}, 15000);
-	
-	// Keep alive (keep the player active)
-	var inQueue = 0;
-	setInterval(function(){
-		if (inQueue > 0) {
-			$.post("keepAlive.do");
-		}
-	}, 5000);
-	
-	var changeName = function() {
-		var newName = $("#changeNameForm").find("input[name='nickname']").val();
-		var posting = $.post("changeName.do", { name: newName } );
+// Keep alive (keep the player active)
+var inQueue = 0;
+setInterval(function(){
+	if (inQueue > 0) {
+		$.post("keepAlive.do");
+	}
+}, 5000);
 
-		posting.done(function() {
-		});
-	};
-	
-	var joinQueue = function(queueName) {
-		var queueName = $form.find("input[name='queueName']").val();
-		var posting = $.post("joinQueue.do", { queueName: queueName } );
+var changeName = function() {
+	var newName = $("#changeNameForm").find("input[name='nickname']").val();
+	var posting = $.post("changeName.do", { name: newName } );
 
-		posting.done(function() {
-			inQueue++;
-		});
-	};
-	
-	var quitQueue = function(queueName) {
-		var queueName = $form.find("input[name='queueName']").val();
-		var posting = $.post("quitQueue.do", { queueName: queueName } );
+	posting.done(function() {
+	});
+};
 
-		posting.done(function() {
-			inQueue--;
-		});
-	};
-	
-	var leaveAllQueues = function() {
-		var posting = $.post("quitAllQueues.do");
+var joinQueue = function(queueName) {
+	var queueName = $form.find("input[name='queueName']").val();
+	var posting = $.post("joinQueue.do", { queueName: queueName } );
 
-		posting.done(function() {
-			inQueue = 0;
-		});
-	};
-	
-	var createQueue = function() {
-		var queueName = $("#changeNameForm").find("input[name='queueName']").val();
-		var queueMaxSize = $("#changeNameForm").find("input[name='queueMaxSize']").val();
-		var posting = $.post("createQueue.do", { queueName: queueName, maxSize: queueMaxSize } );
+	posting.done(function() {
+		inQueue++;
+	});
+};
 
-		posting.done(function() {
-		});
-	};
-	
-	var deleteQueue = function() {
-		var queueName = $("#changeNameForm").find("input[name='queueName']").val();
-		var posting = $.post("deleteQueue.do", { queueName: queueName } );
+var quitQueue = function(queueName) {
+	var queueName = $form.find("input[name='queueName']").val();
+	var posting = $.post("quitQueue.do", { queueName: queueName } );
 
-		posting.done(function() {
-		});
-	};
-	
-</script>
+	posting.done(function() {
+		inQueue--;
+	});
+};
+
+var leaveAllQueues = function() {
+	var posting = $.post("quitAllQueues.do");
+
+	posting.done(function() {
+		inQueue = 0;
+	});
+};
+
+var createQueue = function() {
+	var queueName = $("#changeNameForm").find("input[name='queueName']").val();
+	var queueMaxSize = $("#changeNameForm").find("input[name='queueMaxSize']").val();
+	var posting = $.post("createQueue.do", { queueName: queueName, maxSize: queueMaxSize } );
+
+	posting.done(function() {
+	});
+};
+
+var deleteQueue = function() {
+	var queueName = $("#changeNameForm").find("input[name='queueName']").val();
+	var posting = $.post("deleteQueue.do", { queueName: queueName } );
+
+	posting.done(function() {
+	});
+};
