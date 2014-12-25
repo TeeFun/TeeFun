9bd17eb60483a7b740a008f510a4d5c3035d4817
Rajh <Hetmoteus@gmail.com>
2014-12-26 00:52:20 +0100

    Started Security

--- src/main/java/com/teeworlds/teefun/context/WebSecurityConfig.java
+++ src/main/java/com/teeworlds/teefun/context/WebSecurityConfig.java
@@ -3,6 +3,14 @@
  */
 package com.teeworlds.teefun.context;
 
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,11 +21,30 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @author Rajh
  *
  */
+@Configuration
 @EnableWebSecurity
+@EnableGlobalAuthentication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
 		http.headers().xssProtection();
+		http.csrf().disable().authorizeRequests().anyRequest().authenticated();
+	}
+
+	@Resource
+	public void registerGlobal(final AuthenticationManagerBuilder auth) throws Exception {
+		// @formatter:off
+		auth
+		.inMemoryAuthentication()
+		.withUser("user").password("password").roles("USER").and()
+		.withUser("admin").password("password").roles("USER", "ADMIN");
+		// @formatter:on
 	}
 }
